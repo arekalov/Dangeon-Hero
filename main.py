92b1904d943909dd428e51d3d8c8df47b907b819
arekalov <artyom.rekalov@gmail.com>
2022-01-22 14:01:33 +0400

    Added sprite's mechanic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import random
 
 import pygame as pg
@@ -6,42 +7,110 @@ import sys
 import pygame.display
 from pygame.sprite import Sprite
 
-W = 700
-H = 500
+W = 1000
+H = 510
 sc = pg.display.set_mode((W, H))
 clock = pygame.time.Clock()
 fps = 60
 
-hero_group = pygame.sprite.Group()
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data/textures', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
 
 
-class Hero(pygame.sprite.Sprite):
-    image = pygame.image.load("data\\characters_sprites\\hero-1.png")
+def load_level(filename):
+    filename = "data/levels/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
 
-    def __init__(self):
-        super(Hero, self).__init__(hero_group)
-        self.image = Hero.image
-        self.rect = self.image.get_rect()
-        self.rect.x = 10
-        self.rect.y = 10
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
+tile_images = {
+    'und_block': load_image('und_block.png'),
+    'left_block': load_image('left_block.png'),
+    'right_block': load_image('right_block.png'),
+    'bwg1': load_image('block_with_green.png'),
+    'billboard': load_image('billboard.png'),
+    'bwg2': load_image('block_with_green2.png'),
+    'bwg3': load_image('block_with_green3.png'),
+    'chest': load_image('chest.png'),
+    'opened_chest': load_image('opened_chest.png'),
+    'diamond': load_image('diamond.png'),
+    'flowers': load_image('flowers.png'),
+    'ladder': load_image('ladder.png'),
+    'thorn': load_image('thorn.png')
+}
+# player_image = load_image('mario.png')
+tile_width, tile_height = 23, 23
 
-cntrl = False
-hero = Hero()
+tiles_group = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '0':
+                Tile('left_block', x, y)
+            elif level[y][x] == '9':
+                Tile('right_block', x, y)
+            elif level[y][x] == '1':
+                Tile('bwg1', x, y)
+            elif level[y][x] == 'u':
+                Tile('und_block', x, y)
+            elif level[y][x] == 'b':
+                Tile('billboard', x, y)
+            elif level[y][x] == '2':
+                Tile('bwg2', x, y)
+            elif level[y][x] == '3':
+                Tile('bwg3', x, y)
+            elif level[y][x] == 'c':
+                Tile('chest', x, y)
+            elif level[y][x] == 'o':
+                Tile('opened_chest', x, y)
+            elif level[y][x] == 'd':
+                Tile('diamond', x, y)
+            elif level[y][x] == 'f':
+                Tile('flowers', x, y)
+            elif level[y][x] == 'l':
+                Tile('ladder', x, y)
+            elif level[y][x] == 't':
+                Tile('thorn', x, y)
+    #         elif level[y][x] == '@':
+    #             Tile('empty', x, y)
+    #             new_player = Player(x, y)
+    # # вернем игрока, а также размер поля в клетках
+    return x, y
+
+
+level_x, level_y = generate_level(load_level('level.txt'))
+
 while 1:
     for i in pg.event.get():
         if i.type == pg.QUIT:
             sys.exit()
-        if pygame.key.get_pressed()[pygame.K_RIGHT]:
-            hero.rect.x += 10
-        if pygame.key.get_pressed()[pygame.K_LEFT]:
-            hero.rect.x -= 10
-        if pygame.key.get_pressed()[pygame.K_UP]:
-            hero.rect.y -= 10
-        if pygame.key.get_pressed()[pygame.K_DOWN]:
-            hero.rect.y += 10
     pygame.display.flip()
     sc.fill((0, 0, 0))
-    hero_group.draw(sc)
+    all_sprites.draw(sc)
     clock.tick(fps)
